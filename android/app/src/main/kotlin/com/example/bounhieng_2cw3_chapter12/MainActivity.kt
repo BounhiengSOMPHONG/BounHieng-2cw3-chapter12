a5846cce39200b88527e03ecffb3be0bb933be87
BounHiengSomphong <teng.ios123@gmail.com>
2024-01-17 23:46:08 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/bounhieng_2cw3_chapter12/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bounhieng_2cw3_chapter12
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:bounhieng_2cw3_chapter12/Client%20Platform%20Channel/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: home(),
+    );
+  }
+}
